b05cf8031f075283e979f7a6a4fa5a4f7beba453
Simon <simon.lejeune@mink-agency.com>
2020-06-10 20:09:58 +0200

    :sparkles: Add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [
+    "./pages/**/*.jsx",
+    "./pages/**/*.mdx",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "typescript": "#294E80",
+        "angular": "#d60034",
+        "react": "#61dafb",
+        "vuejs": "#4fc08d",
+        "laravel": "#e74430",
+        "nestjs": "#ea2845",
+        "svelte": "#ff3e00",
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
